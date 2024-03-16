fecfad53d5fa6db38a6e0dac32543a18fe8d0f7b
Rajan <rajanj448@student.sfit.ac.in>
2024-03-16 18:07:51 +0530

    Dark & Light Theme setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,12 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-  <h1>Discord</h1>
+  <div>
+    <UserButton afterSignOutUrl="/" />
+    <ModeToggle />
+  </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ConverseConnect",
@@ -17,8 +19,21 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-       <html lang="en">
-      <body className={inter.className}>{children}</body>
+       <html lang="en" suppressHydrationWarning>
+       <body className={cn(
+          font.className,
+          "bg-white dark:bg-[#313338]"
+        )}>
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+       
+        enableSystem
+        storageKey="discord-theme"
+        >
+        {children}
+        </ThemeProvider>
+      </body>
     </html>
     </ClerkProvider>
   
